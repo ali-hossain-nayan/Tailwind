175329c55ed8903f173ba9d7666735c9a3d82a29
Nayan1234l <ali15-13943@diu.edu.bd>
2024-03-20 01:01:48 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./dist/practice.html"],
+  darkMode: "class",
+  theme: {
+    extend: {
+      colors: {
+        chesnat: '#087505'
+      }
+    },
+  },
+  plugins: [],
+}
